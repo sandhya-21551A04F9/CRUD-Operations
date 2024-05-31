2ac3cad8e50d4a142859a8c3b181e88ccc573c01
satyavolu sandhya <152674923+sandhya-21551A04F9@users.noreply.github.com>
2024-05-31 18:19:55 +0530

    crud operations

---
+++ newproject/src/employee/deletedata.java
@@ -0,0 +1,62 @@
+package employee;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class deletedata {   
+	
+	public static final String Driver = "com.mysql.cj.jdbc.Driver";
+	public static final String Username = "root";
+	public static final String Password = "root";
+	public static final String Url = "";
+	public static Connection conn;
+	public static PreparedStatement pmst;
+	
+			
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter database name:");
+			
+			String Url = "jdbc:mysql://localhost:3306/" +sc.next();
+			
+			Class.forName(Driver);
+ 			
+			Connection conn = DriverManager.getConnection( Url, Username, Password);
+			
+			System.out.println("Enter table name:");
+			
+			String sql ="delete from " + sc.next()+ " where id = ?";
+			
+			pmst = conn.prepareStatement(sql);
+			
+			System.out.println("Enter id");
+			
+			pmst.setInt(1, sc.nextInt());
+			
+			int i = pmst.executeUpdate();
+			
+			if (i>0) {
+				System.out.println("data deleted succesfully");
+				
+			} else {
+				System.out.println("error");
+
+			}
+			conn.close();
+			pmst.close();
+			sc.close();
+			
+		} catch (Exception e) {
+		 e.printStackTrace();
+		 
+		}
+	
+	}
+
+}
